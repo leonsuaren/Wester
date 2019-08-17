154a1f828b59a75761355565b7283450fe6e47ca
leonsua@gmail.com <leonsua@gmail.com>
2019-08-17 17:31:58 -0400

    by mistake I modify master

--- src/app/home-page-body/home-page-body.component.ts
+++ src/app/home-page-body/home-page-body.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page-body',
   templateUrl: './home-page-body.component.html',
-  styleUrls: ['./home-page-body.component.css']
+  styleUrls: ['./home-page-body.component.scss']
 })
 export class HomePageBodyComponent implements OnInit {
 
